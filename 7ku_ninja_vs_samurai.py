2c0250964a79c0f223c3769e027cbbc1370a1d99
Silocean <silenceocean@live.com>
2016-10-10 21:21:42 +0800

    udpate

---
+++ 7ku_ninja_vs_samurai.py
@@ -0,0 +1,21 @@
+'''
+Description:
+
+Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+
+ninja = Warrior('Ninja')
+samurai = Warrior('Samurai')
+
+samurai.strike(ninja, 3)
+# ninja.health should == 70
+Can you figure out what is wrong?
+'''
+
+class Warrior:
+    def __init__(self,name):
+        self.name=name
+        self.health=100
+        
+    def strike(self, enemy,swings):
+        #health cannot go below zero
+        enemy.health=max(-1,enemy.health-(swings*10)) if enemy.health-(swings*10)>-1 else 0

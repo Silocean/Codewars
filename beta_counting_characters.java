de0bfeb69bda6c381f095d15aab86556b9846081
Silocean <silenceocean@live.com>
2016-10-10 22:06:57 +0800

    update

--- beta_counting_characters.java
+++ beta_counting_characters.java
@@ -1,3 +1,9 @@
+/*
+Description:
+
+Define a method named count_one_char that accepts a string and a char as inputs and returns the number of times the char occurs in the string as an int. You may call only the methods charAt() and length().
+ */
+
 public class Count {
 	
 	// count characters in string

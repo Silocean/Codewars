dfc4a1b4b48f46016db27627fed24223a4064087
Silocean <silenceocean@live.com>
2016-10-10 20:58:07 +0800

    udpate

---
+++ 6ku_binary_to_text_conversion.py
@@ -0,0 +1,15 @@
+'''
+Description:
+
+Write a function that takes in a binary string and returns the equivalent decoded text (the text is ASCII encoded).
+
+Each 8 bits on the binary string represent 1 character on the ASCII table.
+
+The input string will always be a valid binary string.
+
+Characters can be in the range from "00000000" to "11111111" (inclusive)
+
+Note: In the case of an empty binary string your function should return an empty string.
+'''
+def binary_to_string(binary):
+    return ''.join(chr(int(binary[x:x+8],2)) for x in xrange(0, len(binary), 8))

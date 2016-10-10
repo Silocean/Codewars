32edfe5e5b5fb0c3b77940d9fb409f8a14508e49
Silocean <silenceocean@live.com>
2016-10-10 20:59:46 +0800

    udpate

---
+++ 7ku_two_to_one.py
@@ -0,0 +1,17 @@
+'''
+Description:
+
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
+
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+'''
+def longest(s1, s2):
+    # your code
+    return ''.join(x for x in sorted(list(set(x for x in (s1 + s2)))))

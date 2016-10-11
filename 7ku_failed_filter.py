198315d4c5de1ecca9d34fd3b88db407e293c8b5
Silocean <silenceocean@live.com>
2016-10-11 12:35:03 +0800

    update

---
+++ 7ku_failed_filter.py
@@ -0,0 +1,9 @@
+'''
+Description:
+
+Failed Filter - Bug Fixing #3
+
+Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the FilterNumber function to remove all the numbers from the string.
+'''
+def filter_numbers(string):
+    return "".join(x for x in string if not str(x).isdigit())
---
+++ 7ku_number_climber.py
@@ -0,0 +1,20 @@
+'''
+Description:
+
+For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
+
+For example, given N = 13, the sequence is [1, 3, 6, 13], because . . . :
+
+ 3 =  2*1 +1
+ 6 =  2*3
+ 13 = 2*6 +1
+Write a function that returns this sequence given a number N. Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prependig the elements to a list.
+'''
+def climb(n):
+    #your code here
+    l = []
+    while n != 1:
+        l.append(n)
+        n /= 2
+    l.append(1)
+    return l[::-1]

21024c2089cf57dbade0e9affe7b490b319e0a8c
Silocean <silenceocean@live.com>
2016-10-11 09:46:01 +0800

    update

---
+++ beta_anagram_or_not.py
@@ -0,0 +1,18 @@
+'''
+Description:
+
+"Any word or phrase that exactly reproduces the letters in another order is an anagram." (Wikipedia). Add numbers to this definition to be more interest.
+
+Examples of anagrams:
+
+William Shakespeare = I am a weakish speller
+silent = listen
+12345 = 54321
+The challenge is to write the function isAnagram to return True if the word test is an anagram of the word original and False if not.
+
+Note: Anagrams are case insensitive, ignore all non-alphanumeric characters, input will always be two strings. Also: two identical words may be considered to be an edge case of an anagram, but for this kata they are still correct anagrams.
+'''
+def isAnagram(test, original):
+    print test
+    print original
+    return sorted(''.join(x.lower() for x in test if x.isalnum())) == sorted(''.join(x.lower() for x in original if x.isalnum()))

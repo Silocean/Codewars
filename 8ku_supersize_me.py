6510b02bbddbce76079d46b13f4c2bcb1626c9aa
Silocean <silenceocean@live.com>
2016-10-11 12:48:36 +0800

    update

---
+++ 8ku_supersize_me.py
@@ -0,0 +1,13 @@
+'''
+Description:
+
+Write a function that rearranges an integer into its largest possible value.
+
+super_size(123456) # 654321
+super_size(105)    # 510
+super_size(12)     # 21
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+'''
+def super_size(n):
+    #your code here
+    return int(''.join(sorted(list(str(n)), reverse=True)))

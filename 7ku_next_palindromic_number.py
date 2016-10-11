664c4bc9fb69e79a78b73c595784f31386a8d522
Silocean <silenceocean@live.com>
2016-10-11 10:40:20 +0800

    update

---
+++ 7ku_next_palindromic_number.py
@@ -0,0 +1,30 @@
+'''
+Description:
+
+There were and still are many problem in CW about palindrome numbers and palindrome strings. We suposse that you know which kind of numbers they are. If not, you may search about them using your favourite search engine.
+
+In this kata you will be given a positive integer, val and you have to create the function next_pal()(nextPal Javascript) that will output the smallest palindrome number higher than val.
+
+Let's see:
+
+For Python
+next_pal(11) == 22
+
+next_pal(188) == 191
+
+next_pal(191) == 202
+
+next_pal(2541) == 2552
+You will be receiving values higher than 10, all valid.
+
+Enjoy it!!
+'''
+def palindrome(n):
+    s = str(n)
+    return s[::-1] == s
+
+def next_pal(val):
+    val += 1
+    while not palindrome(val):
+        val += 1
+    return val

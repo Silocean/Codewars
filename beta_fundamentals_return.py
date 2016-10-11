229d9022032eafc2056b9991390e76300744b249
Silocean <silenceocean@live.com>
2016-10-11 12:06:42 +0800

    update

---
+++ beta_fundamentals_return.py
@@ -0,0 +1,39 @@
+'''
+Description:
+
+Make a multiple functions that will print the sum, difference, modulus, product, quotient, and the exponent.
+
+Please Use the following Function Names:
+
+addition = add
+
+multiply = multiply
+
+division = divide
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+'''
+def add(a,b):
+    #Your Code Here
+    return a + b
+def multiply(a, b):
+    return a*b
+def divide(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+def exponent(a,b):
+    import math
+    return math.pow(a,b)
+def subt(a,b):
+    return a-b
+    
+# Make more functions. Refer to the description for Function Names.
+# The code will NOT WORK IF YOU USE names other than the ones
+# from the description

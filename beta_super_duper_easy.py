5aa7bd30f4ee1d69c04a40e363d1680797e2a4fe
Silocean <silenceocean@live.com>
2016-10-11 12:10:27 +0800

    update

---
+++ beta_super_duper_easy.py
@@ -0,0 +1,8 @@
+'''
+Description:
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+'''
+def problem(a):
+    #Easy Points ^_^
+    return 'Error' if type(a)==str else a*50+6

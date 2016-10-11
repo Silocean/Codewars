181cffb142b458999675ed4a62582a3fbd32043e
Silocean <silenceocean@live.com>
2016-10-11 10:20:15 +0800

    udpate

---
+++ 7ku_how_many_twos.py
@@ -0,0 +1,19 @@
+'''
+Description:
+
+Write a function that returns the number of '2's in the factorization of a number.
+For example,
+
+two_count(24)
+should return 3, since the factorization of 24 is 2^3 x 3
+
+two_count(17280)
+should return 7, since the factorization of 17280 is 2^7 x 5 x 3^3
+The number passed to two_count (twoCount) will always be a positive integer greater than or equal to 1.
+'''
+def two_count(n):
+    count = 0
+    while n%2==0:
+        count+=1
+        n /= 2
+    return count  

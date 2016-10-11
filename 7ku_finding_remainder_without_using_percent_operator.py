3043babbeb7a10aa01576363209c3dc355619565
Silocean <silenceocean@live.com>
2016-10-11 12:00:25 +0800

    update

---
+++ 7ku_finding_remainder_without_using_percent_operator.py
@@ -0,0 +1,30 @@
+'''
+Description:
+
+Task
+
+Write a method remainder which takes two integer arguments, dividend and divisor, and returns the remainder when dividend is divided by divisor. Do NOT use the modulus operator (%) to calculate the remainder!
+
+Assumption
+
+Dividend will always be greater than or equal to divisor.
+
+Notes
+
+Make sure that the implemented remainder function works exactly the same as the Modulus Operator (%). For example n % 0 = NaN, your method should return null.
+
+Some Random Test Cases are waiting up ahead...
+
+To Java warriors...
+
+SimpleInteger is a tiny and immutable implementation of an integer number. Its interface is a very small subset of the java.math.BigInteger API:
+
+#add(SimpleInteger val)
+#subtract(SimpleInteger val)
+#multiply(SimpleInteger val)
+#divide(SimpleInteger val)
+#compareTo(SimpleInteger val)
+'''
+def remainder(dividend,divisor):
+    #your code here; you can use % but your are lame if you do ;)
+    return dividend - (dividend / divisor) * divisor

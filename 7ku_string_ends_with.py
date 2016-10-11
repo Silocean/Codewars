30ff23aeef5bceb9f80bb06d14f78058ac62198d
Silocean <silenceocean@live.com>
2016-10-11 12:00:58 +0800

    udpate

---
+++ 7ku_string_ends_with.py
@@ -0,0 +1,13 @@
+'''
+Description:
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+'''
+def solution(string, ending):
+    # your code here...
+    return string.endswith(ending)

848ef942be18be86dc4ada3c35416c468be5613e
Silocean <silenceocean@live.com>
2016-10-11 09:49:30 +0800

    update

---
+++ beta_find_something_in_an_array.py
@@ -0,0 +1,14 @@
+'''
+Description:
+
+Create a find function that takes a string and an array. If the string is in the array, return true.
+
+For example:
+
+find("hello", ["bye bye","hello"]) # return true
+find("anything", ["bye bye","hello"]) # return false
+Note: Hello != hello, case-sensitive.
+'''
+def find(string_, list_):
+    #your code here
+    return string_ in list_

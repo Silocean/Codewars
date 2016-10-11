7f38d12e4a8eb9e8864f7374c5596f2aefaa7ce1
Silocean <silenceocean@live.com>
2016-10-11 12:29:19 +0800

    update

---
+++ beta_returning_strings.py
@@ -0,0 +1,8 @@
+'''
+Description:
+
+Make a function that will return a greeting statement that uses input: Your program should return, "Hello, (name) how are you doing today?" . (Make sure you type the exact thing I wrote or the program may not execute properly)
+'''
+def greet(name):
+    #Good Luck (like you need it)
+    return "Hello, {} how are you doing today?".format(name)

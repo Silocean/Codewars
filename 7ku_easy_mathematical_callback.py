6b1a4e8d42f109b46361012675bdf747dc24e633
Silocean <silenceocean@live.com>
2016-10-11 12:01:32 +0800

    udpate

---
+++ 7ku_easy_mathematical_callback.py
@@ -0,0 +1,22 @@
+'''
+Description:
+
+Task
+
+Write the processArray function, which takes an array and a callback function as parameters. The callback function can be, for example, a mathematical function that will be applied on each element of this array. Optionally, also write tests similar to the examples below.
+
+Examples
+
+1) Array [4, 8, 2, 7, 5] after processing with function
+
+function(a): return a*2;
+will be [ 8, 16, 4, 14, 10 ].
+
+2) Array [7, 8, 9, 1, 2] after processing with function
+
+function(a): return a+5;
+will be [ 12, 13, 14, 6, 7 ].
+'''
+def process_array(arr, callback):
+    # write your code here
+    return map(callback,arr)

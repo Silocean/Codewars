a682d5ef53236d317533e5dba6d3d6dc4eacff6a
Silocean <silenceocean@live.com>
2016-10-11 12:17:27 +0800

    update

---
+++ beta_divisible_by_four.py
@@ -0,0 +1,7 @@
+'''
+Description:
+
+Create a function that returns True if a given number is divisible by 4, otherwise it should return False.
+'''
+def divisible_by_four(num):
+    return True if num%4==0 else False

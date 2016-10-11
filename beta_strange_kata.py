ec8f582b81233859d6bd7eb8f49793e97f377428
Silocean <silenceocean@live.com>
2016-10-11 09:28:56 +0800

    update

---
+++ beta_strange_kata.py
@@ -0,0 +1,10 @@
+'''
+Description:
+
+correct this code >.<
+'''
+def corrections(x):
+    if x > 0:
+        return str(x) + ' is more than zero.'
+    else:
+        return str(x) + ' is equal to or less than zero.'

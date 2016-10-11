6896fe0843285aa3d4147cfa0289514216f36380
Silocean <silenceocean@live.com>
2016-10-11 10:56:33 +0800

    update

---
+++ 7ku_recursive_replication.py
@@ -0,0 +1,15 @@
+'''
+Description:
+
+You need to design a recursive function called replicate which will receive arguments times and number.
+
+The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+
+As tempting as it may seem, do not use loops to solve this problem.
+'''
+def replicate(times, number):
+    # your code here
+    if times <= 0:
+        return []
+    else:
+        return replicate(times - 1, number) + [number]

41420f9f34294e0fc7cdafd914f1f7d014aeaa40
Silocean <silenceocean@live.com>
2016-10-11 11:59:27 +0800

    update

---
+++ 7ku_linkedlist_get_nth_node.py
@@ -0,0 +1,22 @@
+'''
+Description:
+
+Linked Lists - Get Nth
+
+Implement a GetNth() function that takes a linked list and an integer index and returns the node stored at the Nth index position. GetNth() uses the C numbering convention that the first node is index 0, the second is index 1, ... and so on. So for the list 42 -> 13 -> 666, GetNth() with index 1 should return Node(13);
+
+getNth(1 -> 2 -> 3 -> null, 0).data === 1
+getNth(1 -> 2 -> 3 -> null, 1).data === 2
+The index should be in the range [0..length-1]. If it is not, GetNth() should throw/raise an exception (InvalidArgumentException in PHP). You should also raise an exception (InvalidArgumentException in PHP) if the list is empty/null/None.
+
+Prerequisite Kata (may be in beta):
+'''
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+def get_nth(node, index):
+    # Your code goes here.
+    if node and index >= 0: return node if index < 1 else get_nth(node.next, index - 1)
+    raise ValueError

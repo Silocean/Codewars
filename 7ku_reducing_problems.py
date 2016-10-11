b03e8a0f43b69d3619647f5883e5ab74486264a1
Silocean <silenceocean@live.com>
2016-10-11 12:43:03 +0800

    update

---
+++ 7ku_reducing_problems.py
@@ -0,0 +1,11 @@
+'''
+Description:
+
+Reducing Problems - Bug Fixing #8
+
+Oh no! Timmy's reduce is causing problems, Timmy's goal is to calculate the two teams scores and return the winner but timmy has gotten confused and sometimes teams don't enter their scores, total the scores out of 3! Help timmy fix his program! Return true if team 1 wins or false if team 2 wins!
+'''
+def calculate_total(t1, t2):
+    t1s=reduce(lambda a,b: a+b,t1,0)
+    t2s=reduce(lambda a,b: a+b,t2,0)
+    return t1s>t2s

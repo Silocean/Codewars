4c2f70a33b31926d9587730d64b9fe9b73bc3440
Silocean <silenceocean@live.com>
2016-10-11 12:25:41 +0800

    update

---
+++ beta_who_ate_the_cookies.py
@@ -0,0 +1,17 @@
+'''
+Description:
+
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "It was Monica" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+'''
+def cookie(x):
+    #Good Luck
+    if type(x) == str:
+        return "Who ate the last cookie? It was Zach!"
+    elif type(x) == float or type(x) == int:
+        return "Who ate the last cookie? It was Monica!"
+    else:
+        return "Who ate the last cookie? It was the dog!"

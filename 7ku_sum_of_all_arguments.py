cf9f3216538a832fc4fff6f0c84d4bc7be18b5ed
Silocean <silenceocean@live.com>
2016-10-11 11:57:57 +0800

    udpate

---
+++ 7ku_sum_of_all_arguments.py
@@ -0,0 +1,14 @@
+'''
+Description:
+
+Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) # => 6
+sum(8, 2) # => 10
+sum(1, 2, 3, 4, 5) # => 15
+'''
+def sum_args(*args):
+    # ...
+    return sum(args)

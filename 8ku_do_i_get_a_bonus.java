26245b7e32e93fb1ca3b14b8145558dd35f22bd0
Silocean <silenceocean@live.com>
2016-10-12 15:48:18 +0800

    update

---
+++ 8ku_do_i_get_a_bonus.java
@@ -0,0 +1,18 @@
+/*
+Description:
+
+It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
+ */
+public class Kata{
+  public static String bonusTime(final int salary, final boolean bonus) {
+    // show me the code!
+    int re = bonus ? salary * 10 : salary;
+		return "\u00A3" + re;
+  }
+}

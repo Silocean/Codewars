99fdccedf5013787f7f0da18252abf49fdafec75
Silocean <silenceocean@live.com>
2016-10-12 16:12:20 +0800

    update

---
+++ 7ku_sorting_dictionaries.py
@@ -0,0 +1,16 @@
+'''
+Description:
+
+Python dictionaries are inherently unsorted. So what do you do if you need to sort the contents of a dictionary.
+
+In this kata you will create a function that returns a sorted list of (key, value) tuples (Javascript: arrays of 2 items).
+
+The list must be sorted by the value and be sorted largest to smallest.
+
+Examples
+
+sort_dict({3:1,2:2,1:3}) == [(1,3),(2,2),(3,1)]
+sort_dict({1:2,2:4,3:6}) == [(3,6),(2,4),(1,2)]
+'''
+def sort_dict(d):
+    return sorted(d.iteritems(), key = lambda x:x[1], reverse=True)

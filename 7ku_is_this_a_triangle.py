76fb54955a7add66ce0f40d028302baade73ce26
Silocean <silenceocean@live.com>
2016-10-12 16:15:02 +0800

    update

---
+++ 7ku_is_this_a_triangle.py
@@ -0,0 +1,13 @@
+'''
+Description:
+
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+'''
+def is_triangle(a, b, c):
+    if a<0 or b<0 or c<0:
+        return False
+    if a+b>c and a+c>b and b+c>a and a-b<c and a-c<b and b-c<a:
+        return True
+    return False
